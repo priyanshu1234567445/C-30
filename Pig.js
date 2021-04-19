04e65de0e959cd683dffae97a994b92ad6de7077
priyanshu1234567445 <79846865+priyanshu1234567445@users.noreply.github.com>
2021-04-19 19:48:57 +0530

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,24 @@
+class Pig extends BaseClass {
+  constructor(x, y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/enemy.png");
+  }
+display() {
+  console.log(this.body.speed);
+  if((this.body.speed) <3){
+    super.display();
+
+  }
+
+else{
+//remove piggoted from the world
+  World.remove(world,this.body);
+  push();
+  this.Visibility = this.Visibility -3;
+  tint(255,this.Visibility);
+  image(this.image,this.body.position.x - this.body.position.y,50,50);
+  pop();
+
+}
+}
+};
